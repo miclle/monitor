769d88222bf1fce4c6b1fd94d7a8d90d649fb829
Miclle <miclle.zheng@gmail.com>
2016-05-23 13:49:55 +0800

    Update monitor main func

--- monitor/main.go
+++ monitor/main.go
@@ -2,15 +2,31 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/carlescere/scheduler"
 )
 
 func main() {
-	job := func() {
-		fmt.Println("Time's up!")
-	}
-	scheduler.Every(5).Seconds().Run(job)
-	scheduler.Every().Day().Run(job)
-	scheduler.Every().Sunday().At("08:30").Run(job)
+	fmt.Println("Main execute at:\t", time.Now())
+
+	scheduler.Every(5).Seconds().Run(func() {
+		fmt.Println("Execute every 5 seconds\t", time.Now())
+	})
+
+	scheduler.Every(1).Minutes().Run(func() {
+		fmt.Println("Execute every 1 minute\t", time.Now())
+	})
+
+	scheduler.Every().Day().Run(func() {
+		fmt.Println("Execute every 1 day\t", time.Now())
+	})
+
+	scheduler.Every().Sunday().At("08:30").Run(func() {
+		fmt.Println("Execute every Sunday 08:30\t", time.Now())
+	})
+
+	done := make(chan struct{})
+
+	<-done
 }
